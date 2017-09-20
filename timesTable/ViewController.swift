46ca28e591e402e67570bf5c1c7f5353eddb390a
darrensill1304 <darrensill1304@gmail.com>
2017-09-20 21:49:07 +0100

    initial commit
    initial commit

--- timesTable/ViewController.swift
+++ timesTable/ViewController.swift
@@ -8,8 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func onButton(_ sender: Any) {
+        
+        print("hello")
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((indexPath.row + 1) * (Int(slider.value)))
+        return cell
+    }
+    
+    @IBAction func onSliderChanged(_ sender: Any) {
+        
+        slider.value = floor(slider.value)
+        numberLabel.text = String(Int(slider.value))
+        
+        table.reloadData()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
